46c2dcc7c29269b6316432730ca4bb29aef4c6d5
profduality <professor.duality@gmail.com>
2019-11-12 18:04:58 -0500

    Changed prompt, alert and declared new variable

--- buttonNamePrompt.js
+++ buttonNamePrompt.js
@@ -1,6 +1,7 @@
 const button = document.querySelector('button');
 
 button.onclick = function() {
-  let name = prompt('What is your name?');
-  alert('Hello ' + name + ', nice to see you!');
+  let name = prompt('What is your favorite animal?');
+  let ready = name.toLowerCase(); //Converts the string to lowercase so anyway the user types it will work in the sentence which the page returns.
+  alert('The ' + ready + ' is a very cool favorite animal!'); //Uses the lowercase string in a basic sentence.
 }
