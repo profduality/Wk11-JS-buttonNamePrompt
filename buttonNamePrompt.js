b85ae4e187f282a67b16f2173a06339abcdf7ea8
profduality <professor.duality@gmail.com>
2019-11-12 18:07:03 -0500

    Added new wording

--- buttonNamePrompt.js
+++ buttonNamePrompt.js
@@ -3,5 +3,5 @@ const button = document.querySelector('button');
 button.onclick = function() {
   let name = prompt('What is your favorite animal?');
   let ready = name.toLowerCase(); //Converts the string to lowercase so anyway the user types it will work in the sentence which the page returns.
-  alert('The ' + ready + ' is a very cool favorite animal!'); //Uses the lowercase string in a basic sentence.
+  alert('A ' + ready + ' is a very cool favorite animal!'); orin
 }
